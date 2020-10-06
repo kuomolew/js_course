c8155deae7de21815ac2fc5c04490c0ebf317d79
Vladimir Mironov <v.mironov@viseven.com>
2020-10-06 22:26:45 +0300

    02-12

--- 1-basics/assets/scripts/script.js
+++ 1-basics/assets/scripts/script.js
@@ -3,3 +3,11 @@ let currentResult = 0;
 currentResult += 10;
 
 outputResult(currentResult, '');
+
+
+const defaultResult = [1,2];
+console.log(defaultResult);
+let test = defaultResult;
+test.push(3);
+console.log(test);
+console.log(defaultResult);
