3e502da5dad9302a4a03c0f44edf138ced13beaa
Vladimir Mironov <v.mironov@viseven.com>
2020-12-12 17:14:47 +0200

    2-14.2

--- 1-basics/assets/scripts/script.js
+++ 1-basics/assets/scripts/script.js
@@ -5,9 +5,25 @@ currentResult += 10;
 outputResult(currentResult, '');
 
 
-const defaultResult = [1,2];
-console.log(defaultResult);
-let test = defaultResult;
-test.push(3);
-console.log(test);
-console.log(defaultResult);
+// const defaultResult = [1,2];
+// console.log(defaultResult);
+// let test = defaultResult;
+// test.push(3);
+// console.log(test);
+// console.log(defaultResult);
+
+
+///////////////// Problem 1
+const fictionalInput = 14;
+let result;
+
+result = fictionalInput + 18;
+
+result = result - 10;
+result = result * 10;
+result = result / 2;
+
+console.log(fictionalInput);
+console.log(result);
+
+// 2 - 14.2
